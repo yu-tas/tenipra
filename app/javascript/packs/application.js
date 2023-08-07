d279fa9560583162b896bf3b3524847c6ccdcfc1
shintaniyuta <cada3986@yahoo.co.jp>
2023-08-08 06:55:46 +0900

    jquery実装完了 #13

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,11 +2,8 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import 'jquery'
-import 'path/to/your/jquery-dependent-script'
 
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
-
