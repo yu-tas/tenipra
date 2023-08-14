f413ab1882e09fe81787403b5c166135aa6b205a
shintaniyuta <cada3986@yahoo.co.jp>
2023-08-14 13:14:45 +0900

    ユーザーのプロフィール機能終了 #21

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,23 +1,21 @@
 class Users::SessionsController < Devise::SessionsController
-  class Users::SessionsController < Devise::SessionsController
-    # before_action :login_user_cannot, only: :new
-  
-    def guest_sign_in
-      user = User.guest
-      sign_in user
-      redirect_to user_path(user.id)
-    end
-  
-    def guest_admin_sign_in
-      user = User.guest_admin
-      sign_in user
-      redirect_to user_path(user.id)
-    end
-  
-    # def login_user_access_restricted
-    #   if user_signed_in?
-    #     redirect_to menus_path 
-    #   end
-    # end
+  # before_action :login_user_cannot, only: :new
+
+  def guest_sign_in
+    user = User.guest
+    sign_in user
+    redirect_to user_path(user.id)
   end
+
+  def guest_admin_sign_in
+    user = User.guest_admin
+    sign_in user
+    redirect_to user_path(user.id)
+  end
+
+  # def login_user_access_restricted
+  #   if user_signed_in?
+  #     redirect_to menus_path 
+  #   end
+  # end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,46 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_user
+  before_action :confirm_correct_user, only: [:edit, :update]
+
   def show
     @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
+    if @user == current_user
+      render "edit"
+    else
+      redirect_to users_path
+    end
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      bypass_sign_in(@user) if params[:user][:password].present?
+      redirect_to user_path(@user), notice: "プロフィールを編集しました！"
+    else
+      flash.now[:danger] = "プロフィールを更新できませんでした"
+      render :edit
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :profile_image)
+  end
+
+  def confirm_correct_user
+    @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to menus_path, alert: "アクセス権限がありません。"
+    end
   end
 end
