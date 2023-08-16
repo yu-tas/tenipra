0fe67b6e3e2e31c479c46223c2a9eba6e3da1021
shintaniyuta <cada3986@yahoo.co.jp>
2023-08-16 21:58:07 +0900

    importscss

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,6 +4,9 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import $ from 'jquery'
 import 'jquery-ui-dist/jquery-ui'
+// app/javascript/packs/application.js
+import '../assets/stylesheets/application.scss';
+
 
 
 
