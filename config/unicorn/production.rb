5ce905da3e34c42227450ab80e8e38c33a0e0549
shintaniyuta <cada3986@yahoo.co.jp>
2023-08-23 19:41:33 +0900

    終了

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -28,6 +28,11 @@ before_fork do |server, worker|
   end
 end
 
+before_exec do |server|
+  ENV["BUNDLE_GEMFILE"] = File.join(File.expand_path("../../../../", __FILE__), "current", "Gemfile")
+end
+
 after_fork do |server, worker|
   defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
 end
+
